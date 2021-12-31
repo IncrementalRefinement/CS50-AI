ad7d738a886ca87a98dea0fbc2679dae09ef7700
unknown <fluent_python@qq.com>
2021-12-31 10:09:04 +0800

    update

--- project/tictactoe/tictactoe.py
+++ project/tictactoe/tictactoe.py
@@ -22,27 +22,51 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    x_num = 0
+    o_num = 0
+    for row in board:
+        for item in row:
+            if item == X:
+                x_num += 1
+            elif item == O:
+                o_num += 1
+    if x_num > o_num:
+        return O
+    else:
+        return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    res = set()
+    for i in range(board.len):
+        for j in range(board.len):
+            if board[i][j] == EMPTY:
+                set.add((i, j))
+    return res
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    possible_actions = action(board)
+    if action not in possible_actions:
+        raise Exception
+    new_board = []
+    for row in board:
+        new_board.append(row.copy())
+    new_board[action[0]][action[1]] = player(board)
+    return new_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+    # TODO: a clean way
     raise NotImplementedError
 
 
@@ -50,14 +74,23 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if (winner(board) is not None) or (len(actions(board)) != 0):
+        return False
+    else:
+        return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    the_winner = winner(board)
+    if the_winner == X:
+        return 1
+    elif the_winner == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
